71af01d74a3b47b29d90cf7802cdbc5b9da459b7
lanthanum55 <lanthanum55@gmail.com>
2025-11-20 21:20:21 +0100

    add L3, L4 labs

---
+++ L3/L3/L3.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <string>
+
+std::mutex m;
+
+int coins = 100;
+const int N = coins; // делаем const, чтобы не изменялось в процессе
+int Bob_coins = 0;
+int Tom_coins = 0;
+
+void coin_sharing(std::string name, int& thief_coins, int& companion_coins) {
+    while (true) {
+        {
+            std::lock_guard<std::mutex> lock(m);
+
+            // Если монеты уже кончились, то выходим
+            if (coins <= 0) {
+                break;
+            }
+
+            // При нечетном кол-ве, отдаем покойнику
+            if (N % 2 != 0 && coins == 1) {
+                std::cout << "===================" << std::endl;
+                std::cout << "*Последняя монета достается покойнику* " << std::endl;
+                coins = 0;
+                break;
+            }
+
+            // Проверка честности дележки
+            if (thief_coins <= companion_coins) {
+                coins--;
+                thief_coins++;
+                std::cout << name << " :" << thief_coins << " " << companion_coins << std::endl;
+            }
+            // Если не может - ничего не делаем
+        }
+        // Небольшая пауза для правильной работы дележки
+        std::this_thread::yield();
+    }
+}
+
+int main() {
+
+    setlocale(LC_ALL, "ru");
+    std::thread bob(coin_sharing, "Bob", std::ref(Bob_coins), std::ref(Tom_coins));
+    std::thread tom(coin_sharing, "Tom", std::ref(Tom_coins), std::ref(Bob_coins));
+
+    bob.join();
+    tom.join();
+
+    std::cout << "\nРезультат:\n";
+    std::cout << "Bob: " << Bob_coins << std::endl;
+    std::cout << "Tom: " << Tom_coins << std::endl;
+    std::cout << "Покойник: " << (N - Bob_coins - Tom_coins) << std::endl;
+
+    return 0;
+}
---
+++ L4/L4/L4.cpp
@@ -0,0 +1,123 @@
+﻿#include <thread>
+#include <mutex>
+#include <vector>
+#include <atomic>
+#include <chrono>
+#include <iostream>
+
+const int NUM_DAYS = 5;
+const int MAX_NUGGETS_PER_eater = 10000;
+
+std::mutex table_lock;
+std::vector<int> bowls = { 3000, 3000, 3000 };
+std::atomic<bool> cook_fired(false);
+std::atomic<int> fatmen_eaten[3] = { 0, 0, 0 };
+std::atomic<bool> has_exploded[3] = { false, false, false };
+std::atomic<int> exploded_count(0);
+std::atomic<bool> time_up(false);
+std::atomic<bool> time_is_up(false);
+
+void eater(int id, int gluttony) {
+    while (!cook_fired.load() && !time_is_up.load()) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::lock_guard<std::mutex> lock(table_lock);
+
+        if (cook_fired.load() || time_is_up.load())
+            return;
+
+        if (bowls[id] <= 0) {
+            cook_fired = true;
+            return;
+        }
+
+        int to_eat = std::min(gluttony, bowls[id]);
+        bowls[id] -= to_eat;
+        fatmen_eaten[id] += to_eat;
+
+
+        if (!has_exploded[id] && fatmen_eaten[id] >= MAX_NUGGETS_PER_eater) {
+            has_exploded[id] = true;
+            exploded_count++;
+        }
+    }
+}
+
+void cook(int efficiency_factor) {
+    auto start = std::chrono::steady_clock::now();
+    const auto duration = std::chrono::seconds(NUM_DAYS);
+
+    while (!cook_fired.load()) {
+        auto now = std::chrono::steady_clock::now();
+        if (now - start >= duration) {
+            break;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        if (cook_fired.load()) break;
+
+        std::lock_guard<std::mutex> lock(table_lock);
+        if (cook_fired.load()) break;
+
+        for (int i = 0; i < 3; ++i) {
+            bowls[i] += efficiency_factor;
+        }
+    }
+    time_is_up = true; // даём сигнал остановиться
+}
+
+void run_test(int gluttony, int efficiency_factor, const std::string& name) {
+    //сброс
+    bowls = { 3000, 3000, 3000 };
+    cook_fired = false;
+    time_is_up = false;
+    exploded_count = 0;
+    for (int i = 0; i < 3; ++i) has_exploded[i] = false;
+    for (int i = 0; i < 3; ++i) fatmen_eaten[i] = 0;
+
+
+    std::cout << "\n=== " << name << " ===\n";
+    std::cout << "обжорство = " << gluttony << ", продуктивность кука = " << efficiency_factor << "\n";
+
+    std::thread t_cook(cook, efficiency_factor);
+    std::thread t1(eater, 0, gluttony);
+    std::thread t2(eater, 1, gluttony);
+    std::thread t3(eater, 2, gluttony);
+
+    t_cook.join();
+    t1.join();
+    t2.join();
+    t3.join();
+
+    // результат
+    if (cook_fired) {
+        std::cout << "результат: кука уволили\n";
+    }
+    else if (exploded_count == 3) {
+        std::cout << "результат: кук не получил зарплату\n";
+    }
+    else {
+        std::cout << "результат: кук уволился сам\n";
+    }
+
+    std::cout << "в тарелках у обжор: ";
+    for (int p : bowls) std::cout << p << " ";
+    std::cout << std::endl;
+    std::cout << "обжоры съели: ";
+    for (int i = 0; i < 3; i++) std::cout << fatmen_eaten[i] << " ";
+    std::cout << std::endl;
+    std::cout << "взорвалось обжор: " << exploded_count << "\n";
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    // Набор коэффициентов 1:
+    run_test(500, 100, "условие 1: увольнение");
+
+    // Набор коэффициентов 2:
+    run_test(220, 800, "условие 2: кук не получил зарплату");
+
+    // Набор коэффициентов 3:
+    run_test(200, 500, "условие 3: увольняется сам");
+
+    return 0;
+}
